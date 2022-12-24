5d0e16a6037e2e4b1674bd6f5bddb2261cb22a71
PRABAKARANM123 <113657482+PRABAKARANM123@users.noreply.github.com>
2022-12-24 14:47:27 +0530

    Add files via upload
    Model object replaced by @ModelAttribute annotation

--- spring-love-calculater-java-based/src/main/java/com/msp/lc/controller/LCAppController.java
+++ spring-love-calculater-java-based/src/main/java/com/msp/lc/controller/LCAppController.java
@@ -2,6 +2,7 @@ package com.msp.lc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,15 +11,13 @@ import com.msp.lc.api.UserInfoDTO;
 public class LCAppController {
 	
 	@RequestMapping("/")
-	public String homePage(Model model) {
-		UserInfoDTO userInfo = new UserInfoDTO();
-		model.addAttribute("userInfo", userInfo);
+	public String homePage(@ModelAttribute("userInfo") UserInfoDTO userInfo) {
+		
 		return "home-page";
 	}
 	
 	@RequestMapping("/proces-homepage")
-	public String resultPage(UserInfoDTO userInfoDTO, Model model) {
-		model.addAttribute("userinfo", userInfoDTO);
+	public String resultPage(@ModelAttribute("userinfo") UserInfoDTO userinfo) {
 		
 		return "result-page";
 	}
