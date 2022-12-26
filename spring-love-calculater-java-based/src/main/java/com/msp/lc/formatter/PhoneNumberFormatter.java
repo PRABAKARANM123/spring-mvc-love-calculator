b63561fdef172a715cea5fe8b97a401db1411d61
PRABAKARANM123 <113657482+PRABAKARANM123@users.noreply.github.com>
2022-12-26 20:29:59 +0530

    Added '91-' by default if user not entered

--- spring-love-calculater-java-based/src/main/java/com/msp/lc/formatter/PhoneNumberFormatter.java
+++ spring-love-calculater-java-based/src/main/java/com/msp/lc/formatter/PhoneNumberFormatter.java
@@ -17,14 +17,30 @@ public class PhoneNumberFormatter implements Formatter<Phone>{
 
 	@Override
 	public Phone parse(String compleatPhoneNumber, Locale locale) throws ParseException {
-		// TODO Auto-generated method stub
-		System.out.println("Inside the parse method of formatter");
+		Phone phone = new Phone();
+
+		System.out.println("Inside the parse method of  the PhoneNumberFormatter");
 		// Split the String received from the User
 		String[] phoneNumberArray = compleatPhoneNumber.split("-");
 		// Extract the CountryCode and set it to the Phone Class country code property
-		Phone phone = new Phone();
-		phone.setCountryCode(phoneNumberArray[0]);
-		phone.setUserNumber(phoneNumberArray[1]);
+
+		// whether the number consist of -
+		int index = compleatPhoneNumber.indexOf('-');
+		if (index == -1 || compleatPhoneNumber.startsWith("-")) {
+			// if the '-' not found, then add 91 as the default country code
+			phone.setCountryCode("91");
+			if (compleatPhoneNumber.startsWith("-")) {
+
+				phone.setUserNumber(phoneNumberArray[1]);
+			} else {
+
+				phone.setCountryCode(phoneNumberArray[0]);
+
+			}
+		} else {
+			phone.setCountryCode(phoneNumberArray[0]);
+			phone.setUserNumber(phoneNumberArray[1]);
+		}
 		return phone;
 	}
 
